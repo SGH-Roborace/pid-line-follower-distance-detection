ec96296075174a958b8aa989417fc9206f1888b2
kklub123 <kiko3.141528@gmail.com.com>
2023-11-09 23:12:42 +0100

    not quite happy with where we are now thinking about using a pid next

--- main.py
+++ main.py
@@ -20,35 +20,42 @@ ev3 = EV3Brick()
 drife = DriveBase(Motor(Port.B), Motor(Port.A), 43, 88)
 farbe = ColorSensor(Port.S1)
 weg = UltrasonicSensor(Port.S2)
-zickzack = [50,50,50,50,50,50,50,50,50,50,50]
+zickzack = 50
 '''
 while True:
-    zickzack.append(farbe.reflection())
-    bong = sum(zickzack) / len(zickzack)
+    zickzack = zickzack*1/3 farbe.reflection()*2/3
+    
     bing = weg.distance()
 
-    drife.drive(max((bing)-abs(bong),0), (bong-50)*3.6 )
+    drife.drive(max((bing)-abs(zickzack),0), (zickzack-50)*3.6 )
     zickzack.pop(0)
 '''
+
 while True:
     # Value of white 54 value of black 8 difference 46
     # linear steering from 70 degrees
     # distance between 15 and 25
     distance = weg.distance()
     reflection = farbe.reflection()
+    '''
     if distance > 250:
         distance = 250
     elif distance < 150:
         distance = 150
 
 
-    if reflection > 54:
-        reflection = 54
+    if reflection > 80:
+        reflection = 80
     elif reflection < 8:
         reflection = 8
-    speed = (distance-150) * 2 # results in speed between 0 and 300
-    angle = (reflection-8-23) * -2
-    drife.drive(speed, angle)
-
-    
-
+    '''
+    speed = int((distance-150) * 3 - abs((reflection-8-35)/35)**3*100) # results in speed between 0 and 300
+    angle = (reflection-8-36) * -2.4
+    drife.drive(max(speed, 0), angle)
+'''
+while True:
+    ev3.screen.clear()
+    reflection = farbe.reflection()
+    ev3.screen.draw_text(0,0,str(abs((reflection-8-35)/35)**2*170))
+    time.sleep(1)
+    '''
